cc5023c2ffa718f906d38cf7e704fcfbf5ec40ae
Priscila Roza <priscilarozasilva@gmail.com>
2022-07-23 17:39:41 -0300

    Criação das teclas da calculadora

--- src/components/Calculadora.vue
+++ src/components/Calculadora.vue
@@ -1,17 +1,24 @@
 <template>
   <div class="calculadora">
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
+    <div class="display">25</div>
+    <div>C</div>
+    <div>+/-</div>
+    <div>%</div>
+    <div>÷</div>
+    <div>7</div>
+    <div>8</div>
+    <div>9</div>
+    <div>x</div>
+    <div>4</div>
+    <div>5</div>
+    <div>6</div>
+    <div>-</div>
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+    <div>+</div>
+    <div>0</div>
+    <div>=</div>
   </div>
 </template>
 
@@ -22,8 +29,16 @@ export default {};
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .calculadora {
+  font-size: 40px;
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-auto-rows: minmax(50px, auto);
+}
+.display{
+  /*para especificar em qual coluna colocar um item e quantas colunas o item abrangerá.*/
+  grid-column: 1 / 5;
+  background-color: #333;
+  opacity: 0.6;
+  color: #fff;
 }
 </style>
